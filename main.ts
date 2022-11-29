d688dd8f83fcf86dc6cb4bc4f436472c06d8cc56
Martins Zeltins <queeeeenz@gmail.com>
2022-11-29 22:58:06 +0200

    Check memory consumption

--- main.ts
+++ main.ts
@@ -1,33 +1,13 @@
-import util from 'util'
-import pidusage from 'pidusage'
-import { exec as _exec } from 'child_process'
+import { getProcessIds, checkResourceConsumption } from './util'
 
-const PROCESS_NAMES = ['gnome-calculator', 'gnome-calculator']
-
-let PROCESS_IDS: string[] = []
-
-const exec = util.promisify(_exec)
-
-const getProcessIds = async () => {
-    let pids: string[] = []
-
-    for (const processName of PROCESS_NAMES) {
-        const { stdout } = await exec(`pidof ${processName}`);
-        
-        pids.push(stdout.replace('\n', ''))
-    }
-
-    return pids
-}
+const PROCESS_NAMES = ['kdenlive', 'kdenlive']
 
 const main = async () => {
-    PROCESS_IDS = await getProcessIds()
+    const pids = await getProcessIds(PROCESS_NAMES)
 
-    console.log(PROCESS_IDS)
-
-    // pidusage(process.pid, function (_, stats) {
-    //     console.log(stats)
-    // })
+    for (const pid of pids) {
+        await checkResourceConsumption(pid, PROCESS_NAMES)
+    }
 }
 
 main()
---
+++ util.ts
@@ -0,0 +1,81 @@
+import util from 'util'
+import { exec as _exec } from 'child_process'
+
+const exec = util.promisify(_exec)
+
+const getProcessIds = async (processNames: string[]) => {
+    let pids: string[] = []
+
+    for (const processName of processNames) {
+        try {
+            const { stdout } = await exec(`pidof ${processName}`);
+            const pid = stdout.replace('\n', '')
+            const isSinglePid = !stdout.includes(' ')
+    
+            /**
+             * Add the found process id to the list only if there was a
+             * single pid found for the process name and if it has not
+             * been already added.
+             */
+            if (isSinglePid && !pids.includes(pid)) {
+                pids.push(stdout.replace('\n', ''))
+            }
+        } catch (error) {
+            console.log('[info] This process is not running.')
+        }
+    }
+
+    return pids
+}
+
+const getMemoryUsage = async (pid: string) => {
+    const { stdout } = await exec(`ps -p ${pid} -o %mem | grep -o "[0-9].*[0-9]"`)
+
+    return Number(stdout.replace('\n', ''))
+}
+
+const getCpuUsage = async (pid: string) => {
+    const { stdout } = await exec(`ps -p ${pid} -o %cpu | grep -o "[0-9].*[0-9]"`)
+
+    return Number(stdout.replace('\n', ''))
+}
+
+const killAllProcesses = async (processNames: string[]) => {
+    let killCommand = ''
+
+    for (const [index, processName] of processNames.entries()) {
+        killCommand += `killall ${processName}`
+
+        if ((index + 1) < processNames.length) {
+            killCommand += ' && '
+        }
+    }
+
+    console.log(`[cmd] ${killCommand}`)
+    // await exec(killCommand)
+}
+
+const checkResourceConsumption = async (pid: string, processNames: string[]) => {
+    try {
+        // CPU and RAM usages in % of total available
+        const memoryUsage = await getMemoryUsage(pid)
+        const cpuUsage = await getCpuUsage(pid)
+
+        if (memoryUsage > 0 || cpuUsage > 0) {
+            console.log(`[info] Memory: ${memoryUsage}%, CPU: ${cpuUsage}%`)
+            console.log('[info] Resource limit reached. Killing all processes...')
+
+            await killAllProcesses(processNames)
+        }
+    } catch (error) {
+        console.log('[error] check_resource_consumption failed.')
+    }
+}
+
+export {
+    getProcessIds,
+    getMemoryUsage,
+    getCpuUsage,
+    killAllProcesses,
+    checkResourceConsumption
+}
